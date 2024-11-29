aaabe694b749b76a00248272f7db0009988c43e4
grantcore <grantcore@hotmail.com>
2024-11-30 07:02:24 +1100

    Update script.js

--- script.js
+++ script.js
@@ -8,30 +8,8 @@ setInterval(() => {
   totalTimer.textContent = `${minutes}:${seconds}`;
 }, 1000);
 
-// Rest Timer Functionality
-function startRestTimer(button) {
-  const restTime = button.getAttribute("data-time");
-  let timeLeft = parseInt(restTime);
-  if (button.textContent.includes("Start")) {
-    button.textContent = `${timeLeft}s Rest (Stop)`;
-    const timer = setInterval(() => {
-      timeLeft--;
-      button.textContent = `${timeLeft}s Rest (Stop)`;
-      if (timeLeft <= 0) {
-        clearInterval(timer);
-        button.textContent = `${restTime}s Rest (Start)`;
-      }
-    }, 1000);
-    button.onclick = () => {
-      clearInterval(timer);
-      button.textContent = `${restTime}s Rest (Start)`;
-      button.onclick = () => startRestTimer(button);
-    };
-  }
-}
-
-// Finish Workout
+// Finish Workout Function
 function finishWorkout() {
   alert("Workout Finished!");
-  // Logic to save workout data (to be implemented with Firebase)
+  // Logic to save workout data goes here
 }
